0b246d1d54a34f08bd854a1b9fe911ade1109768
tian zhou <zhoutian@storswift.com>
2024-01-12 17:29:13 +0800

    wheres jojo

--- src/hooks/msRecords.ts
+++ src/hooks/msRecords.ts
@@ -4,8 +4,8 @@ export const useMsRecords = (address?: string) => {
   const { records } = useRecords({
     filter: {
       programIds: [
-        'wheres_alex_v018.aleo',
-        'puzzle_pieces_v015.aleo',
+        'wheres_jojo_v008.aleo',
+        'jojo_pieces_v007.aleo',
         'multiparty_pvp_utils_v015.aleo',
       ],
       type: 'unspent',
@@ -14,10 +14,10 @@ export const useMsRecords = (address?: string) => {
     multisig: true,
   });
   const msGameRecords = records?.filter(
-    (record) => record.programId === 'wheres_alex_v018.aleo'
+    (record) => record.programId === 'wheres_jojo_v008.aleo'
   );
   const msPuzzleRecords = records?.filter(
-    (record) => record.programId === 'puzzle_pieces_v015.aleo'
+    (record) => record.programId === 'jojo_pieces_v007.aleo'
   );
   const msUtilRecords = records?.filter(
     (record) => record.programId === 'multiparty_pvp_utils_v015.aleo'
--- src/hooks/records.ts
+++ src/hooks/records.ts
@@ -4,8 +4,8 @@ export const useGameRecords = () => {
   const { records } = useRecords({
     filter: {
       programIds: [
-        'wheres_alex_v018.aleo',
-        'puzzle_pieces_v015.aleo',
+        'wheres_jojo_v008.aleo',
+        'jojo_pieces_v007.aleo',
         'multiparty_pvp_utils_v015.aleo',
       ],
       type: 'unspent',
@@ -13,10 +13,10 @@ export const useGameRecords = () => {
     multisig: false,
   });
   const gameNotifications = records?.filter(
-    (record) => record.programId === 'wheres_alex_v018.aleo'
+    (record) => record.programId === 'wheres_jojo_v008.aleo'
   );
   const puzzleRecords = records?.filter(
-    (record) => record.programId === 'puzzle_pieces_v015.aleo'
+    (record) => record.programId === 'jojo_pieces_v007.aleo'
   );
   const utilRecords = records?.filter(
     (record) => record.programId === 'multiparty_pvp_utils_v015.aleo'
